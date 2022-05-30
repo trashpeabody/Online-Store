9c550ab32cb5c8b63a1a475eff5e81b99195dd51
Jorge Sainz <bodybluebloody@gmail.com>
2022-05-30 10:31:38 +0200

    First steps: configuring and installing everything needed

---
+++ src/App.js
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <h1>Holi</h1>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+
+const jsRules = {
+  test: /\.js$/,
+  loader: 'babel-loader',
+  options: {
+    presets: [
+      [
+        '@babel/preset-react',
+        {
+          runtime: 'automatic'
+        }
+      ]
+    ]
+  }
+}
+
+const sassRules = {
+  test: /\.s[ac]ss$/i,
+  use: [
+    'style-loader',
+    'css-loader',
+    'sass-loader'
+  ]
+}
+
+const imageRules = {
+  test: /\.(png|jpg|jpeg|gif|svg)$/i,
+  type: 'asset/resource'
+}
+
+module.exports = (env, argv) => {
+  const { mode } = argv
+  const isProduction = mode === 'production'
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: isProduction
+        ? '[name].[contenthash].js'
+        : 'main.js',
+      path: path.resolve(__dirname, 'build/')
+    },
+    module: {
+      rules: [jsRules, sassRules, imageRules]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: 'src/index.html' }),
+      new Dotenv({ systemvars: true })
+    ],
+    devServer: {
+      open: true,
+      historyApiFallback: true
+    }
+  }
+}
